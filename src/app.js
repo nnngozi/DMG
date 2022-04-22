5bfcd4034f9847eae65bcad068f4d4c80be98f92
nnngozi <nnngozi@gmail.com>
2022-04-22 18:05:02 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -9,3 +9,31 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+function dmg() {
+  var pronoun = ["The", "our", "your", "my"];
+  let adj = ["great", "big", "small", "tall"];
+  let noun = ["jogger", "baller", "miami", "florida"];
+  let suffix = [".com", ".net", ".us", ".me"];
+
+  var randomPronoun = Math.floor(Math.random() * pronoun.length);
+  var randomAdj = Math.floor(Math.random() * adj.length);
+  var randomNoun = Math.floor(Math.random() * noun.length);
+  var randomSuffix = Math.floor(Math.random() * suffix.length);
+
+  for (var p = 0; p < pronoun.length; p++) {
+    for (var a = 0; a < adj.length; a++) {
+      for (var n = 0; n < noun.length; n++) {
+        for (var s = 0; s < suffix.length; s++) {
+
+          return (
+            pronoun[randomPronoun]+adj[randomAdj] +noun[randomNoun] + suffix[randomSuffix]
+          );
+
+        }
+      }
+    }
+
+  }
+
+}  
+console.log(dmg ());
